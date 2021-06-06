13d17daa7bd3548ea15101ca4b61008ecb4623c9
raviteja <ravitej059@gmail.com>
2021-06-06 16:18:46 +0530

    addded content in echo.sh file

--- echo.sh
+++ echo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-hai hello world
-echo -e "Hello raviteja/n"
+#hai hello world
+#echo -e "Hello raviteja/n"
 
-echo-e "welocome to devops trainee/n"
+#echo-e "welocome to devops trainee/n"
 
-echo-e hello
-echo -e   123456
-echo -e "/e[31m hello in red color"]
+#echo-e hello
+#echo -e   123456
+#echo -e "/e[31m hello in red color
+# echo -e "/[32m hello in green colo"]
