7a2a3711c55f4705cd6cffb604007cab183f4e7e
raviteja <ravitej059@gmail.com>
2021-06-17 18:29:19 +0530

    addded content

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -6,3 +6,8 @@ HEAD "Installing  Nginx"
 yum install  ngnix -y &>>/tmp/roboshop.log
 echo -e "\e[32m done\e[0m"
 STAT $?
+
+HEAD "Start Nginx"
+systemctl start nginx &>>/tmp/roboshop.log
+systemctl start enable nginx &>>.tmp/roboshop.log
+STAT $?
