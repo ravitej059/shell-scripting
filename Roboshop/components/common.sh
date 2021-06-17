754b259dc9a9f5c614de5b0a574637fe0b896b28
raviteja <ravitej059@gmail.com>
2021-06-17 18:09:45 +0530

    addded content

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -1,3 +1,14 @@
 HEAD() {
   echo  -n -e "\e[1m $1 \e[0m \t\t ..."
 }
+STAT()
+{
+  if [$1-eq0]; then
+    echo-e "/e[1;32m done\e[0m"
+    else
+      echo -e "\e[1;31m fail\e[0m"
+      echo -e "\t \e[1;33m chek the log for detail ... Log File :/rmp/roboshop.log/e[0m"
+      exit 1
+      fi
+
+}
--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -5,3 +5,4 @@ source components/common.sh
 HEAD "Installing  Nginx"
 yum install  ngnix -y &>>/tmp/roboshop.log
 echo -e "\e[32m done\e[0m"
+STAT $?
