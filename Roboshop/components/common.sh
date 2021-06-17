d0cec94dfb1dd9337ba2b6e0271a1bb4850eb9ea
raviteja <ravitej059@gmail.com>
2021-06-17 18:51:33 +0530

    addded content

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -1,14 +1,13 @@
 HEAD() {
-  echo  -n -e "\e[1m $1 \e[0m \t\t ..."
+  echo -n -e "\e[1m $1 \e[0m \t\t ... "
 }
-STAT()
-{
-  if [ $1 -eq0 ]; then
+
+STAT() {
+  if [ $1 -eq 0 ]; then
     echo -e "\e[1;32m done\e[0m"
   else
-      echo -e "\e[1;31m fail\e[0m"
-      echo -e "\e[1;33m check the log for detail ... Log-File :/rmp/roboshop.log\e[0m"
-      exit 1
+    echo -e "\e[1;31m fail\e[0m"
+    echo -e "\e[1;33m Check the log for more detail ... Log-File : /tmp/roboshop.log\e[0m"
+    exit 1
   fi
-
-}
+  }
