a7815165489fe4c647690fe34f6aa592daa44bb0
raviteja <ravitej059@gmail.com>
2021-06-17 18:42:16 +0530

    addded content

--- Roboshop/components/common.sh
+++ Roboshop/components/common.sh
@@ -4,7 +4,7 @@ HEAD() {
 STAT()
 {
   if [ $1 -eq0 ]; then
-    echo-e "\e[1;32m done\e[0m"
+    echo -e "\e[1;32m done\e[0m"
   else
       echo -e "\e[1;31m fail\e[0m"
       echo -e "\e[1;33m check the log for detail ... Log-File :/rmp/roboshop.log\e[0m"
