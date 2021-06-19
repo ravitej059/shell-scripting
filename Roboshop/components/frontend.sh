454804d63470ab191298c6444bc3d2904a569e7c
raviteja <ravitej059@gmail.com>
2021-06-19 11:28:40 +0530

    addded content

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -2,16 +2,24 @@
 
 source components/common.sh
 
-HEAD "Installing  Nginx"
+HEAD "Installing  Nginx\t"
 yum install  ngnix -y &>>/tmp/roboshop.log
 echo -e "\e[32m done\e[0m"
 STAT $?
 
-HEAD "Start Nginx\t"
+HEAD "Start Nginx\t\t"
 systemctl start nginx &>>/tmp/roboshop.log
 systemctl  enable nginx &>>/tmp/roboshop.log
 STAT $?
 
-HEAD  "Download from Github"
+HEAD  "Download from Github\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Delete old HTML Docs\t"
+rm -rf /usr/share/ngix/html/*
+STAT $?
+
+HEAD "Extract Downloaded content"
+unzip -d /usr/share/ngix/html/tmp/frontend.zip  &>>/tmp/roboshop.log
 STAT $?
