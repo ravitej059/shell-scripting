ad0060c6dfad0a58c7ba1ab51858588d4df92ead
raviteja <ravitej059@gmail.com>
2021-06-19 11:17:26 +0530

    addded content

--- Roboshop/components/frontend.sh
+++ Roboshop/components/frontend.sh
@@ -11,3 +11,7 @@ HEAD "Start Nginx\t"
 systemctl start nginx &>>/tmp/roboshop.log
 systemctl  enable nginx &>>/tmp/roboshop.log
 STAT $?
+
+HEAD  "Download from Github"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
