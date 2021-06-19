5267768047a3c598cd88572b409058350ec32738
raviteja <ravitej059@gmail.com>
2021-06-19 11:51:13 +0530

    addded content

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -26,17 +26,3 @@ mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/robosho
 mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Update Nginx Configuration"
-mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
-STAT $?
-
-HEAD "Update Endpoints in Nginx Config"
-for component in catalogue cart user shipping payment ; do
-  sed -i -e "/${component}/ s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
-done
-STAT $?
-
-HEAD "Start Nginx\t\t"
-systemctl restart nginx &>>/tmp/roboshop.log
-systemctl enable nginx &>>/tmp/roboshop.log
-STAT $?
