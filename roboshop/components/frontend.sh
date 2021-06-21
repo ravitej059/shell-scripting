cc282784e98cb174b39de2c3b1106dfa813712c6
raviteja <ravitej059@gmail.com>
2021-06-21 10:29:42 +0530

    addded content

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,25 +1,39 @@
+
 #!/bin/bash
 
 source components/common.sh
+rm -f /tmp/roboshop.log
+set-hostname frontend
 
-HEAD "Installing  Nginx\t"
-yum install  ngnix -y &>>/tmp/roboshop.log
-echo -e "\e[1;32m done\e[0m"
+HEAD "Installing Nginx\t"
+yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Start Nginx\t\t"
-systemctl start nginx &>>/tmp/roboshop.log
-systemctl  enable nginx &>>/tmp/roboshop.log
-STAT $?
-
-HEAD  "Download from Github\t"
+HEAD "Download from GitHub\t"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Delete old HTML Docs\t"
-rm -rf /usr/share/ngix/html/*
+HEAD "Delete old HTML DOCS\t"
+rm -rf /usr/share/nginx/html/*
 STAT $?
 
 HEAD "Extract Downloaded Content"
-unzip -d /usr/share/nginx/html /tmp/frontend.zip -o &>>/tmp/roboshop.log
+unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Update Nginx Configuration"
+mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
+
+HEAD "Update Endpoints in Nginx Config"
+for component in catalogue cart user shipping payment ; do
+  sed -i -e "/${component}/ s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done
+STAT $?
+
+HEAD "Start Nginx\t\t"
+systemctl restart nginx &>>/tmp/roboshop.log
+systemctl enable nginx &>>/tmp/roboshop.log
 STAT $?
