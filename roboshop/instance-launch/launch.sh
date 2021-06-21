009632a2a8f64681f267e6eeb16442772ae5a661
raviteja <ravitej059@gmail.com>
2021-06-21 16:33:02 +0530

    addded content

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -10,8 +10,6 @@ fi
 LID=lt-08c82b1b813a01076
 LVER=1
 
-
-
  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
   if [ "${INSTANCE_STATE}" = "running" ]; then
     echo "${COMPONENT} Instance already exists!!"
@@ -22,3 +20,5 @@ LVER=1
     echo "${COMPONENT} Instance already exists!!"
     return 0
   fi
+
+  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
