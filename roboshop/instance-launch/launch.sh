eefc86870cd854f238c8c310d8b511a0862a4819
raviteja <ravitej059@gmail.com>
2021-06-21 15:40:21 +0530

    addded content

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -8,6 +8,6 @@ if [ -z "${COMPONENT}" ];  then
 fi
 
 LID=lt-08c82b1b813a01076
-LVer=1
+LVER=1
 
-aws ec2 run-instances     --launch-template LaunchTemplateId=${LTD},Version=${LVR} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+aws ec2 run-instances     --launch-template LaunchTemplateId=${LTD},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
