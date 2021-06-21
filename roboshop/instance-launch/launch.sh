1c9775e630378f92d0f85d29709800621d9051cb
raviteja <ravitej059@gmail.com>
2021-06-21 15:47:31 +0530

    addded content

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -10,4 +10,4 @@ fi
 LID=lt-08c82b1b813a01076
 LVER=1
 
-aws ec2 run-instances     --launch-template LaunchTemplateId=${LTD},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+aws ec2 run-instances     --launch-template LaunchTemplateId=${LTD},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
