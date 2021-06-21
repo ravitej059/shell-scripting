d69d1d14a222ffe96fa787f29a4a6b1c76cf5769
raviteja <ravitej059@gmail.com>
2021-06-21 15:39:33 +0530

    addded content

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -5,7 +5,9 @@ COMPONENT=$1
 if [ -z "${COMPONENT}" ];  then
   echo "Component Input is Needed"
   exit 1
-  fi
+fi
+
 LID=lt-08c82b1b813a01076
 LVer=1
+
 aws ec2 run-instances     --launch-template LaunchTemplateId=${LTD},Version=${LVR} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
