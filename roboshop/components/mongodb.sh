e1541b1b2525b2ab996efcd333dfdaa161cc077d
raviteja <ravitej059@gmail.com>
2021-06-22 12:45:17 +0530

    addded content

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -15,3 +15,25 @@ STAT $?
 HEAD "Install MongoDB\t\t"
 yum install -y mongodb-org &>>/tmp/roboshop.log
 STAT $?
+
+HEAD  "Update Listen Address in config  file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+STAT $?
+
+HEAD "Start MongoDB Service\t\t"
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log
+STAT $?
+HEAD "Download Schema from GitHub\t"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract Downloaded Archive\t"
+cd /tmp
+unzip -o mongodb.zip &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Load Schema\t\t\t"
+cd mongodb-main
+mongo < catalogue.js &>>/tmp/roboshop.log && mongo < users.js &>>/tmp/roboshop.log
+STAT $?
