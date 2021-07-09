4ef4f6279a07a814aaafec4c295f38d00256bf4c
raviteja <ravitej059@gmail.com>
2021-07-09 15:51:57 +0530

    addded content

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -15,7 +15,7 @@ DNS_UPDATE(){
    sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
    aws route53 change-resource-record-sets --hosted-zone-id Z04831613M7X1SNANEAOG --change-batch file:///tmp/record.json | jq
 
-}
+       }
 
  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
   if [ "${INSTANCE_STATE}" = "running" ]; then
