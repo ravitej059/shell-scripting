fce619f13e128fd10d481e29babd35810845f75f
raviteja <ravitej059@gmail.com>
2021-07-12 11:03:40 +0530

    addded content

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -34,12 +34,4 @@ HEAD "Fix Permission to the App Contenet"
 chown roboshop:roboshop /home/roboshop  -R
 STAT $?
 
-HEAD "Setup Systemd Service"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-
-STAT $?
-
-HEAD "Start Catalogue Service"
-systemctl daemon-reload  && systemctl enable catalogue &>>/tmp/roboshop.log && systemctl restart catalogue &>>/tmp/roboshop.log
-STAT $?
 
