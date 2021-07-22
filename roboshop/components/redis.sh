cc787d33696b4746d9556f07aa7968a9e5da5ca9
raviteja <ravitej059@gmail.com>
2021-07-22 12:49:16 +0530

    addded content

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -3,7 +3,5 @@
 source components/common.sh
 rm -f /tmp/roboshop.log
 set-hostname redis
-
-HEAD "Setup Redis Repos"
- yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/roboshop.log && yum-config-manager --enable remi  &>>/tmp/roboshop.log
- STAT $?
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>/tmp/roboshop.log && yum-config-manager --enable remi &>>/tmp/roboshop.log
+STAT $?
